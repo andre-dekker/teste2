49c59ed3bc2d608f1932c8a455ac63eaae02afa3
andre-dekker <anderdekker@gmail.com>
2022-06-15 12:25:41 -0300

    ExercicioPreçoJantar

---
+++ HackerrankTest/TestaPreçoJantar.js
@@ -0,0 +1,10 @@
+function totalPrice(a){
+   var preçoJantar = a;
+   var taxa = a * 0.1;
+   var total = a + a * 0.1;
+   console.log('Valor do jantar: R$ ' + preçoJantar + ',00')
+   console.log('Taxa de Serviço: R$ ' + taxa + ',00')
+   console.log('Total: R$ ' + total + ',00')
+}
+
+totalPrice(80)
