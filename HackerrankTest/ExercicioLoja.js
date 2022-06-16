9333802bf2c577043cebb9452de3c090d1d327dc
andre-dekker <anderdekker@gmail.com>
2022-06-16 01:19:26 -0300

    ExercicioLoja

--- HackerrankTest/ExercicioLoja.js
+++ HackerrankTest/ExercicioLoja.js
@@ -2,8 +2,8 @@ function formaDePagamento(a){
 var preço = a
 var aVista = preço * 0.9
 var Parcelado = preço / 3
-console.log('Preço: R$ ' + preço)
-console.log('A vista: R$ ' + aVista)
-console.log('Parcelado: 3x de R$ ' + Parcelado)
+console.log('Preço: R$ ' + preço.toFixed(2))
+console.log('A vista: R$ ' + aVista.toFixed(2))
+console.log('Parcelado: 3x de R$ ' + Parcelado.toFixed(2))
 }
-formaDePagamento(80)
+formaDePagamento(38)
